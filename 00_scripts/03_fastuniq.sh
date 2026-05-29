be227ab561cf50fb90f7a9ee91c28d3f7a76f150
alexa923 <alexa.martin.thiers@gmail.com>
2026-05-29 15:28:13 +0200

    Update 03_fastuniq.sh

--- 00_scripts/03_fastuniq.sh
+++ 00_scripts/03_fastuniq.sh
@@ -39,13 +39,13 @@ for R1_gz in clean_*_R1.fastq.gz; do
         zcat "$ENTREE/$R1_gz" > "$R1_tmp" 
         zcat "$ENTREE/$R2_gz" > "$R2_tmp"
 
-        echo -e "$R1_tmp\n$R2_tmp" > "$listfile"
+        echo -e "${R1_tmp}\n${R2_tmp}" > "$listfile"
 
         fastuniq -i "$listfile" -t q \
             -o "${SORTIE}/${base}_dedup_R1.fastq" \
             -p "${SORTIE}/${base}_dedup_R2.fastq"
 
-        rm -f "$R1_tmp" "$R2_tmp" "$listfile" 
+        rm -f "$R1_tmp" "$R2_tmp" "$listfile"
 
     else
         echo "ATTENTION: fichier R2 manquant pour $base"
