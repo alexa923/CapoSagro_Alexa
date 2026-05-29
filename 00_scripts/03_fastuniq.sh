fcac43b47e4c375c22b0ee49fc62cca326226e16
alexa923 <alexa.martin.thiers@gmail.com>
2026-05-29 13:43:29 +0200

    Update 03_fastuniq.sh

--- 00_scripts/03_fastuniq.sh
+++ 00_scripts/03_fastuniq.sh
@@ -5,8 +5,8 @@
 #SBATCH --mem=1000G
 #SBATCH --mail-user=alexa.martin@inrae.fr
 #SBATCH --mail-type=ALL 
-#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/03_fastuniq.err"
-#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/03_fastuniq.out"
+#SBATCH --error=/home/amartin3/CapoSagro_Alexa/00_scripts/03_fastuniq.err
+#SBATCH --output=/home/amartin3/CapoSagro_Alexa/00_scripts/03_fastuniq.out
 
 ENTREE="/home/amartin3/02_bbduk"
 SORTIE="/home/amartin3/03_fastuniq"
@@ -17,14 +17,13 @@ module load conda/4.12.0
 source ~/.bashrc
 conda activate bioinformatic
 
-cd "$ENTREE" || exit 1 # terminate a script  (an error has occurred)
+cd "$ENTREE" || exit 1 
 
-TMP="/tmp/fastuniq_tmp" #?
-#créer le dossier s'il n'existe pas deja
+TMP="/tmp/fastuniq_tmp" 
 mkdir -p "$TMP"
 
-for R1_gz in clean_*_R1.fastq.gz; do #on met pas .fastq.gz? 
-    base=$(echo "$R1_gz" | sed 's/_R1\.fastq\.gz//') #la aussi?
+for R1_gz in clean_*_R1.fastq.gz; do
+    base=$(echo "$R1_gz" | sed 's/_R1\.fastq\.gz//')
     R2_gz="${base}_R2.fastq.gz"
 
 
@@ -35,16 +34,16 @@ for R1_gz in clean_*_R1.fastq.gz; do #on met pas .fastq.gz?
         R2_tmp="${TMP}/${base}_R2.fastq"
         listfile="${TMP}/${base}.list"
 
-        zcat "$ENTREE/$R1_gz" > "$R1_tmp" #regarder le contenu des fichiers compressés ?
+        zcat "$ENTREE/$R1_gz" > "$R1_tmp" 
         zcat "$ENTREE/$R2_gz" > "$R2_tmp"
 
         echo -e "$R1_tmp\n$R2_tmp" > "$listfile" #? 
 
-        fastuniq -i "$listfile" -t q \ #lancement de fastuniq?
+        fastuniq -i "$listfile" -t q \ 
             -o "${SORTIE}/${base}_dedup_R1.fastq" \
             -p "${SORTIE}/${base}_dedup_R2.fastq"
 
-        rm -f "$R1_tmp" "$R2_tmp" "$listfile" # suppression définitive? 
+        rm -f "$R1_tmp" "$R2_tmp" "$listfile" 
 
     else
         echo "ATTENTION: fichier R2 manquant pour $base"
