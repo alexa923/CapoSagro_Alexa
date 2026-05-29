856372846b1cc4c4ad26cf54b246115aea97986b
alexa923 <alexa.martin.thiers@gmail.com>
2026-05-29 15:21:58 +0200

    Update 04_clumpify.sh

--- 00_scripts/04_clumpify.sh
+++ 00_scripts/04_clumpify.sh
@@ -11,6 +11,7 @@
 
 ENTREE="/home/amartin3/03_fastuniq"
 SORTIE="/home/amartin3/04_clumpify"
+QUALITE="/home/amartin3/04_clumpify/controle_qualite"
 
 module load conda/4.12.0
 source ~/.bashrc
@@ -19,24 +20,35 @@ conda activate bioinformatic
 CLUMPIFY=clumpify.sh
 
 mkdir -p "$SORTIE"
+mkdir -p "$QUALITE"
 
 
 for R1 in "$ENTREE"/*_R1.fastq; do
     R2="${R1/_R1.fastq/_R2.fastq}"
-    if [[ -f "$R2" ]]; then
-        # Extraire le nom de base pour l'output 
-        base=$(basename "$R1" _R1.fastq) #pourquoi ?
-
-        #Lancement de Clumpify pour chaque paire de reads
+    if [[ -f "$R2" ]]; then 
+        base=$(basename "$R1" _R1.fastq)
         $CLUMPIFY \
             in="$R1" in2="$R2" \
             out="$SORTIE/${base}_clumpify_R1.fastq.gz" \
-            out2="SORTIE/${base}_clumpify_R2.fastq.gz" \
-            dedupe=t #Remove duplicate reads.  For pairs, both must match pourquoi=t
+            out2="$SORTIE/${base}_clumpify_R2.fastq.gz" \
+            dedupe=t
         echo "Clumpify terminé pour $base
     else
-        echo "Fichier R2 manquant pour $R1, ignoré" #comment ça ignoré
-    fi #pourquoi
+        echo "Fichier R2 manquant pour $R1, ignore"
+    fi
 done    
 
+echo "Analyse Clumpify terminee "
+
 #faire controle qualité 
+
+echo "ANALYSE DE LA QUALITE"
+
+
+echo "Lancement de FastQC"
+fastqc "$SORTIE"/*.fastq.gz --outdir "$QUALITE" --threads 4
+
+echo "Lancement de MultiQC"
+multiqc "$QUALITE" "$SORTIE" -o "$QUALITE"
+
+echo "Analyse finalisee"
