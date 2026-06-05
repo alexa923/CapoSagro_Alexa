6616822bc842dc5a37f9494265419b6224e1470c
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-05 10:18:52 +0200

    Update 05_fastp.sh

--- 00_scripts/05_fastp.sh
+++ 00_scripts/05_fastp.sh
@@ -38,30 +38,30 @@ do
 
 #fastp
   fastp \
-        --in1 "$R1" --in2 "$R2" \ 
-        --out1 "$OUT_R1" --out2 "$OUT_R2" \ 
-        --merged_out "$MERGED" \ 
-        --length_required 20 \ 
+        --in1 "$R1" --in2 "$R2" \
+        --out1 "$OUT_R1" --out2 "$OUT_R2" \
+        --merged_out "$MERGED" \
+        --length_required 20 \
         --cut_front --cut_tail \
-        --cut_window_size 4 \ 
+        --cut_window_size 4 \
         --cut_mean_quality 20 \
-        --n_base_limit 5 \ 
+        --n_base_limit 5 \
         --unqualified_percent_limit 40 \
         --low_complexity_threshold 30 \
         --qualified_quality_phred 20 \
-        --low_complexity_filter \ 
-        --trim_poly_x \ 
-        --poly_x_min_len 10 \ 
+        --low_complexity_filter \
+        --trim_poly_x \
+        --poly_x_min_len 10 \
         --merge --correction \
-        --overlap_len_require 30 \ 
-        --overlap_diff_limit 5 \ 
-        --overlap_diff_percent_limit 20 \ 
-        --html "$HTML" \ 
+        --overlap_len_require 30 \
+        --overlap_diff_limit 5 \
+        --overlap_diff_percent_limit 20 \
+        --html "$HTML" \
         --json "$JSON" \
-        --adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \ 
+        --adapter_sequence AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
         --adapter_sequence_r2 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-        --detect_adapter_for_pe \ 
-        --thread 4 
+        --detect_adapter_for_pe \
+        --thread 4
 
    echo "Traitement de $base terminé"
 done
@@ -70,13 +70,13 @@ echo "Tous les traitements sont terminés"
 echo "Lancement de FastQC"
 for fq_gz in "$SORTIE"/*.fastq.gz; do
     if [[ -f "$fq_gz" ]]; then
-        echo "--> FastQC en cours sur : $(basename "$fq_gz")"
+        echo "-> FastQC en cours sur : $(basename "$fq_gz")"
         fastqc "$fq_gz" --outdir "$QUALITE" --threads 4
     fi
 done
 
 echo "Lancement de MultiQC..."
-# Déplacement dans le dossier pour empêcher MultiQC de scanner les gros fichiers de $SORTIE
+
 cd "$QUALITE" || exit 1
 multiqc . -o . --force
 
