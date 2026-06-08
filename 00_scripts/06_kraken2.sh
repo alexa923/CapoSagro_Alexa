21e458ae9209ddec743b15e4b1930a57fdadd9ec
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 09:59:13 +0200

    Update 06_kraken2.sh

--- 00_scripts/06_kraken2.sh
+++ 00_scripts/06_kraken2.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+#SBATCH --job-name=06_krakené
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/plstenge/coprolites/00_scripts/06_kraken2.err"
+#SBATCH --output="/home/plstenge/coprolites/00_scripts/06_kraken2.out"
