03756195c8dfe95895e7d0529910153da6afe30a
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 10:06:39 +0200

    Update and rename 06_kraken2.sh to 07_kraken2.sh

--- 00_scripts/07_kraken2.sh
+++ 00_scripts/07_kraken2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=06_krakené
+#SBATCH --job-name=07_kraken2
 #SBATCH --ntasks=1
 #SBATCH -p smp
 #SBATCH --mem=1000G
 #SBATCH --mail-user=alexa.martin@inrae.fr
 #SBATCH --mail-type=ALL 
-#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/06_kraken2.err"
-#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/06_kraken2.out"
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/07_kraken2.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/07_kraken2.out"
 
 
 module load conda/4.12.0
