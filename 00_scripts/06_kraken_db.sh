2a8c5c192e10f7456880e1f809bec2a40a7577bd
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 10:11:01 +0200

    Update 06_kraken_db.sh

--- 00_scripts/06_kraken_db.sh
+++ 00_scripts/06_kraken_db.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=06_krakené
+#SBATCH --job-name=06_kraken_db
+##SBATCH --time=96:00:00
 #SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
 #SBATCH -p smp
 #SBATCH --mem=1000G
 #SBATCH --mail-user=alexa.martin@inrae.fr
 #SBATCH --mail-type=ALL 
 #SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/06_kraken2.err"
 #SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/06_kraken2.out"
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
