4d9bd3372ac4b395a1fde052281602366c5ce0b4
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 15:14:34 +0200

    Update 07_kraken2.sh

--- 00_scripts/07_kraken2.sh
+++ 00_scripts/07_kraken2.sh
@@ -34,3 +34,22 @@ do
     echo "Termine: $SAMPLE (merged)"
 done 
     
+#Analyse des unmerged (paired end)
+echo "analyse des unmerged"
+
+for R1 in "$ENTREE"/*_fastp_R1.fastq.gz
+do
+    SAMPLE=$(basename "$R1" _fastp_R1.fastq.gz)
+    R2="$ENTREE/${SAMPLE}_fastp_R2.fastq.gz"
+   
+    # Ne lance que si R2 existe
+    if [[ -f "$R2" ]]; then
+        SORTIE_KRAKEN="$SORTIE/${SAMPLE}_unmerged.kraken"
+        SORTIE_REPORT="$SORTIE/${SAMPLE}_unmerged.report"
+
+        kraken2 --conf 0.2 --paired --db "$KRAKEN2_DB" --threads $THREADS \
+            --output "$SORTIE_KRAKEN" --report "$SORTIE_REPORT" "$R1" "$R2"
+
+        echo "Termine : $SAMPLE (unmerged)"
+    fi
+done
