dd3fa8433824297126b5a99be3d2e3dc4ee7027a
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 15:31:24 +0200

    Update 08_bracken.sh

--- 00_scripts/08_bracken.sh
+++ 00_scripts/08_bracken.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=08_bracken
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/08_bracken.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/08_bracken.out"
 
