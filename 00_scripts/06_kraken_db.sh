6e9ffbae54109573f4753120efda438e19e2835f
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 10:05:06 +0200

    Create 06_kraken_db.sh

---
+++ 00_scripts/06_kraken_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=06_krakené
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/06_kraken2.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/06_kraken2.out"
