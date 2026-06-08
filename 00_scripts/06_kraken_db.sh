c480a12ce57d3a530467ac07998fa43b1bc731c9
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-08 10:20:31 +0200

    Update 06_kraken_db.sh

--- 00_scripts/06_kraken_db.sh
+++ 00_scripts/06_kraken_db.sh
@@ -23,4 +23,7 @@ conda activate bioinformatic
 # dead_nucl.accession2taxid.gz
 # ex: wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz
 
-wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz 
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/pdb.accession2taxid.gz 
+wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/dead_nucl.accession2taxid.gz 
