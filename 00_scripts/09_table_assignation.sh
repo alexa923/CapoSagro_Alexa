df56088b1e567ca8e439a6a2ed764b41eee0bf2b
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-10 14:53:04 +0200

    Update 09_table_assignation.sh

--- 00_scripts/09_table_assignation.sh
+++ 00_scripts/09_table_assignation.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=09_table_assignation
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation.out"
 
+
+cd /home/amartin3/08_bracken
+
+#git clone https://github.com/jenniferlu717/KrakenTools.git
+
+python 3  /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r
