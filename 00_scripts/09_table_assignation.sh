cc058b3e20dac5c3f8b5fb5353e75eef7edd4fd3
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-10 15:21:08 +0200

    Update 09_table_assignation.sh

--- 00_scripts/09_table_assignation.sh
+++ 00_scripts/09_table_assignation.sh
@@ -4,12 +4,15 @@
 #SBATCH -p smp
 #SBATCH --mem=1000G
 #SBATCH --mail-user=alexa.martin@inrae.fr
-#SBATCH --mail-type=ALL 
+#SBATCH --mail-type=ALL
 #SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation.err"
 #SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation.out"
 
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
 
-SORTIE="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation"
+SORTIE="/home/amartin3/09_table_assignation"
 cd /home/amartin3/08_bracken
 
 #git clone https://github.com/jenniferlu717/KrakenTools.git
@@ -18,14 +21,15 @@ mkdir -p "$SORTIE"
 
 #Creation de la table d assignation
 echo "creation de la table"
-python 3  /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_merged_bracken.report -o "$SORTIE/clean_sed6_concat_dedup_merged_bracken.mpa"
-python 3  /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_unmerged_bracken.report -o "$SORTIE/clean_sed6_concat_dedup_unmerged_bracken.mpa"
-python 3  /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_merged_bracken.report -o "$SORTIE/clean_sed8_concat_dedup_merged_bracken.mpa"
-python 3  /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_unmerged_bracken.report -o "$SORTIE/clean_sed8_concat_dedup_unmerged_bracken.mpa"
+python3 /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_merged_bracken.report -o "$SORTIE/clean_sed6_concat_dedup_merged_bracken.mpa"
+python3 /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_unmerged_bracken.report -o "$SORTIE/clean_sed6_concat_dedup_unmerged_bracken.mpa"
+python3 /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_merged_bracken.report -o "$SORTIE/clean_sed8_concat_dedup_merged_bracken.mpa"
+python3 /home/amartin3/08_bracken/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_unmerged_bracken.report -o "$SORTIE/clean_sed8_concat_dedup_unmerged_bracken.mpa"
 echo "table creee"
 
 echo "Combinaison en un seul fichier"
-python 3  /home/amartin3/08_bracken/KrakenTools/combine_mpa.py -i clean_sed6_concat_dedup_merged_bracken.mpa clean_sed6_concat_dedup_unmerged_bracken.mpa clean_sed8_concat_dedup_merged_bracken.mpa clean_sed8_concat_dedup_unmerged_bracken.mpa -o "$SORTIE/combined_mpa.tsv"
-
+python3 /home/amartin3/08_bracken/KrakenTools/combine_mpa.py \
+    -i "$SORTIE/clean_sed6_concat_dedup_merged_bracken.mpa","$SORTIE/clean_sed6_concat_dedup_unmerged_bracken.mpa","$SORTIE/clean_sed8_concat_dedup_merged_bracken.mpa","$SORTIE/clean_sed8_concat_dedup_unmerged_bracken.mpa" \
+    -o "$SORTIE/combined_mpa.tsv"
 
 echo "analyse terminee"
