fdb85b995cfcd347f1eb0e722b0d8f9e9a4c1371
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-11 09:48:41 +0200

    Update 10_top50euka.R

--- 00_scripts/10_top50euka.R
+++ 00_scripts/10_top50euka.R
@@ -41,3 +41,71 @@ plot_sed8 <- ggplot(top_sed8, aes(x = sed8total, y = reorder(Taxon, sed8total)))
 
 print(plot_sed8)
 
+########################TOP 30 pour les mammifères, poissons et plantes
+
+library(tidyverse)
+
+# importer les données
+df <- read.csv2("results_table_assignation.csv")  
+df <- df[, 1:7] # Garde uniquement les 7 premières colonnes
+colnames(df) <- c("Taxonomy", "sed6_merge", "sed6_unmerge", "sed8_merge", "sed8_unmerge", "sed6total", "sed8total")
+
+#séparation en 3 tableaux
+
+# tableau mammifères
+df_mammiferes <- df %>%
+  filter(str_detect(Taxonomy, "c__Mammalia") & str_detect(Taxonomy, "\\|s__")) %>%
+  mutate(Taxon = sub(".*\\|s__", "", Taxonomy))
+
+# tableau poissons
+df_poissons <- df %>%
+  filter(str_detect(Taxonomy, "c__Actinopteri") & str_detect(Taxonomy, "\\|s__")) %>%
+  mutate(Taxon = sub(".*\\|s__", "", Taxonomy))
+
+# tableau plantes
+df_plantes <- df %>%
+  filter(str_detect(Taxonomy, "Viridiplantae") & str_detect(Taxonomy, "\\|s__")) %>%
+  mutate(Taxon = sub(".*\\|s__", "", Taxonomy))
+
+
+# Graphiques sed6
+#mammifères
+top_mamm_sed6 <- top_n(df_mammiferes, 30, wt = sed6total)
+plot_mamm_sed6 <- ggplot(top_mamm_sed6, aes(x = sed6total, y = reorder(Taxon, sed6total))) +
+  geom_col(fill = "blue") + labs(title = "SED6 - Top 30 Mammifères", x = "Reads", y = "Espèces") + theme_minimal()
+print(plot_mamm_sed6)
+
+#poisson
+top_pois_sed6 <- top_n(df_poissons, 30, wt = sed6total)
+plot_pois_sed6 <- ggplot(top_pois_sed6, aes(x = sed6total, y = reorder(Taxon, sed6total))) +
+  geom_col(fill = "blue") + labs(title = "SED6 - Top 30 Poissons", x = "Reads", y = "Espèces") + theme_minimal()
+print(plot_pois_sed6)
+
+#plantes
+top_plan_sed6 <- top_n(df_plantes, 30, wt = sed6total)
+plot_plan_sed6 <- ggplot(top_plan_sed6, aes(x = sed6total, y = reorder(Taxon, sed6total))) +
+  geom_col(fill = "blue") + labs(title = "SED6 - Top 30 Plantes", x = "Reads", y = "Espèces") + theme_minimal()
+print(plot_plan_sed6)
+
+
+# Graphiques sed8
+#mammifères
+top_mamm_sed8 <- top_n(df_mammiferes, 30, wt = sed8total)
+plot_mamm_sed8 <- ggplot(top_mamm_sed8, aes(x = sed8total, y = reorder(Taxon, sed8total))) +
+  geom_col(fill = "orange") + labs(title = "SED8 - Top 30 Mammifères", x = "Reads", y = "Espèces") + theme_minimal()
+print(plot_mamm_sed8)
+
+# poissons
+top_pois_sed8 <- top_n(df_poissons, 30, wt = sed8total)
+plot_pois_sed8 <- ggplot(top_pois_sed8, aes(x = sed8total, y = reorder(Taxon, sed8total))) +
+  geom_col(fill = "orange") + labs(title = "SED8 - Top 30 Poissons", x = "Reads", y = "Espèces") + theme_minimal()
+print(plot_pois_sed8)
+
+# plantes
+top_plan_sed8 <- top_n(df_plantes, 30, wt = sed8total)
+plot_plan_sed8 <- ggplot(top_plan_sed8, aes(x = sed8total, y = reorder(Taxon, sed8total))) +
+  geom_col(fill = "orange") + labs(title = "SED8 - Top 30 Plantes", x = "Reads", y = "Espèces") + theme_minimal()
+print(plot_plan_sed8)
+
+
+
