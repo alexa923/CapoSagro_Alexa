bdea1882b507b4a589491d1bc88ab22f17e9639d
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-12 14:24:50 +0200

    Create 11_mapdamage.sh

---
+++ 00_scripts/11_mapdamage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=09_table_assignation
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation.err"
