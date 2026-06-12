67cf47a18d7e22a7e397eb039fd0a21a51f3c69f
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-12 14:50:35 +0200

    Update 11_mapdamage.sh

--- 00_scripts/11_mapdamage.sh
+++ 00_scripts/11_mapdamage.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=09_table_assignation
+#SBATCH --job-name=11_mapdamage
 #SBATCH --ntasks=1
 #SBATCH -p smp
 #SBATCH --mem=1000G
 #SBATCH --mail-user=alexa.martin@inrae.fr
 #SBATCH --mail-type=ALL
 #SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation.err"
+
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
+
+##installer mapdamage_py39
+
+BRACKEN_DIR="home/amartin3/08_bracken"
+FASTQ_DIR="home/amartin3
