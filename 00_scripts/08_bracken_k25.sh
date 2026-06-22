d8efc6de81dc72d911df682c3797c182b177f58e
alexa923 <alexa.martin.thiers@gmail.com>
2026-06-22 08:47:32 +0200

    Update 08_bracken_k25.sh

--- 00_scripts/08_bracken_k25.sh
+++ 00_scripts/08_bracken_k25.sh
@@ -1,1 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=08_bracken_k25
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL 
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/08_bracken_k25.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/08_bracken_k25.out"
 
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
+
+KRAKEN2_DB="/storage/groups/gdec/shared/Kraken_database/k2_core_nt_20250609"
+ENTREE="/home/amartin3/07_kraken2_k25"
+SORTIE="/home/amartin3/08_bracken_k25"
+SORTIE_KRONA="/home/amartin3/08_bracken_k25/krona"
+
+
+mkdir -p "$SORTIE"
+mkdir -p "$SORTIE_KRONA"
+
+for fichier_report in "$ENTREE"/*.report; do
+    
+    # On récupère le nom de l'échantillon
+    base=$(basename "$fichier_report" .report)
+    echo "Traitement Bracken pour : $base"
+    
+    # Lancement de Bracken 
+    echo "lancement de bracken"
+    bracken \
+        -d "$KRAKEN2_DB" \
+        -i "$fichier_report" \
+        -o "$SORTIE/${base}_bracken.txt" \
+        -w "$SORTIE/${base}_bracken.report" \
+        -r 50
+
+    echo "Bracken terminé pour $base"
+   
+done
+
+echo "Bracken termine"
+
+#krona
+echo "Lancement de Krona"
+
+ktImportTaxonomy \
+    -t 5 \
+    -m 3 \
+    -o "$SORTIE_KRONA/multi-krona.html" \
+    "$SORTIE"/*_bracken.report
+
+echo "Graphique Krona termine"
