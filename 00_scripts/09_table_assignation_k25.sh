6dc0232c925a8325af2fe609017490503b80a46b
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-01 15:32:24 +0200

    Update 09_table_assignation_k25.sh

--- 00_scripts/09_table_assignation_k25.sh
+++ 00_scripts/09_table_assignation_k25.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=09_table_assignation_k25
 #SBATCH --ntasks=1
-#SBATCH -p gdec
-#SBATCH --time=10-00:00:00
-#SBATCH --mem=400G
+#SBATCH -p smp
+#SBATCH --mem=1000G
 #SBATCH --mail-user=alexa.martin@inrae.fr
 #SBATCH --mail-type=ALL
 #SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation_k25.err"
@@ -23,10 +22,10 @@ cd /home/amartin3/07_kraken2_k25
 
 #Creation de la table d assignation
 echo "creation de la table"
-python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_merged_kraken.report -o clean_sed6_concat_dedup_merged_kraken.mpa
-python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_unmerged_kraken.report -o clean_sed6_concat_dedup_unmerged_kraken.mpa
-python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_merged_kraken.report -o clean_sed8_concat_dedup_merged_kraken.mpa
-python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_unmerged_kraken.report -o clean_sed8_concat_dedup_unmerged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_merged.report -o clean_sed6_concat_dedup_merged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_unmerged.report -o clean_sed6_concat_dedup_unmerged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_merged.report -o clean_sed8_concat_dedup_merged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k25/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_unmerged_.report -o clean_sed8_concat_dedup_unmerged_kraken.mpa
 echo "table creee"
 
 echo "Combinaison en un seul fichier"
