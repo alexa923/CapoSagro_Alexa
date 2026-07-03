6843a7e6dd4e8268b298e0fbe1ea4fc40e3fc290
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-03 08:54:00 +0200

    Create 07_test_kraken.sh

---
+++ 00_scripts/07_test_kraken.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=repare_sed6_k29
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/repare_sed6_k29.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/repare_sed6_k29.out"
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
+
+# On cible spécifiquement le fichier unique qui a plante
+MERGED="/home/amartin3/05_fastp/clean_sed6_concat_dedup_merged.fastq.gz"
+KRAKEN2_DB="/storage/groups/gdec/shared/Kraken_database/core_nt_k29"
+SORTIE="/home/amartin3/07_kraken2_k29"
+THREADS=36
+
+# Fichiers de sortie (on ajoute .gz pour le fichier kraken)
+SORTIE_REPORT="$SORTIE/clean_sed6_concat_dedup_merged.report"
+SORTIE_KRAKEN="$SORTIE/clean_sed6_concat_dedup_merged.kraken.gz"
+
+echo "Relancement exclusif de sed6_merged avec compression à la volée..."
+
+
+kraken2 --conf 0.2 --db "$KRAKEN2_DB" --threads $THREADS \
+    --output - --report "$SORTIE_REPORT" "$MERGED" | gzip > "$SORTIE_KRAKEN"
+
+echo "Réparation terminée avec succès."
