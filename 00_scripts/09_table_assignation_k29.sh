6967cd2d45d35bf4e92184bb3ccb2b2f37246c14
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-07 14:06:46 +0200

    Create 09_table_assignation_k29.sh

---
+++ 00_scripts/09_table_assignation_k29.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=09_table_assignation_k29
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation_k29.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/09_table_assignation_k29.out"
+
+#################################ASSIGNATION SANS BRACKEN###########################################################################
+
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
+
+
+cd /home/amartin3/07_kraken2_k29
+
+#git clone https://github.com/jenniferlu717/KrakenTools.git
+
+
+#Creation de la table d assignation
+echo "creation de la table"
+python3 /home/amartin3/07_kraken2_k29/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_merged.report -o clean_sed6_concat_dedup_merged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k29/KrakenTools/kreport2mpa.py -r clean_sed6_concat_dedup_unmerged.report -o clean_sed6_concat_dedup_unmerged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k29/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_merged.report -o clean_sed8_concat_dedup_merged_kraken.mpa
+python3 /home/amartin3/07_kraken2_k29/KrakenTools/kreport2mpa.py -r clean_sed8_concat_dedup_unmerged.report -o clean_sed8_concat_dedup_unmerged_kraken.mpa
+echo "table creee"
+
+echo "Combinaison en un seul fichier"
+
+
+python3 /home/amartin3/07_kraken2_k29/KrakenTools/combine_mpa.py -i clean_sed6_concat_dedup_merged_kraken.mpa clean_sed6_concat_dedup_unmerged_kraken.mpa clean_sed8_concat_dedup_merged_kraken.mpa clean_sed8_concat_dedup_unmerged_kraken.mpa -o combined_mpa.tsv
+
+echo "Analyse terminee avec succes !"
