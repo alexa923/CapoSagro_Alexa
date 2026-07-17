c564f202f6777d6a72d266d317d1df4116476026
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-17 10:01:47 +0200

    Update 13_blast_tomate.sh

--- 00_scripts/13_blast_tomate.sh
+++ 00_scripts/13_blast_tomate.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=13_blast_tomate
+#SBATCH --ntasks=1
+#SBATCH -p smp
+#SBATCH --mem=1000G
+#SBATCH --mail-user=alexa.martin@inrae.fr
+#SBATCH --mail-type=ALL
+#SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/13_blast_tomate.err"
+#SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/13_blast_tomate.out"
 
