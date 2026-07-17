071a1ac9631f18c9b01bcefd9b8a8d469302425f
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-17 11:34:35 +0200

    Update 13_blast_tomate.sh

--- 00_scripts/13_blast_tomate.sh
+++ 00_scripts/13_blast_tomate.sh
@@ -19,7 +19,7 @@ KRAKEN_FILE="home/amartin3/07_kraken2/clean_sed6_concat_dedup_merged.kraken"
 FASTQ_FILE="home/amartin3/05_fastp/clean_sed6_concat_dedup_fastp_merged.fastq.gz"
 TAXID=4081
 
-
+cd home/amartin3/07_kraken2
 echo "Début de l'extraction des reads (TaxID: ${TAXID}) - $(date)"
 
 # Extraction des reads avec KrakenTools
@@ -32,7 +32,7 @@ extract_kraken_reads.py \
 
 echo "Extraction terminée. Conversion au format FASTA - $(date)"
 
-# Conversion du FASTQ en FASTA pour le BLAST
-awk 'NR%4==1{sub(/^@/ ,">");print;getline;print}' reads_tomate.fastq > reads_tomate.fasta
+## Conversion du FASTQ en FASTA pour le BLAST
+#awk 'NR%4==1{sub(/^@/ ,">");print;getline;print}' reads_tomate.fastq > reads_tomate.fasta
 
-echo "Conversion terminée. Fin du job - $(date)"
+#echo " Fin du job - $(date)"
