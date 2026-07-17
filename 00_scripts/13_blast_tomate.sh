1805f4c27790c4f86d161f2d16660fbfa6bea2be
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-17 10:13:16 +0200

    Update 13_blast_tomate.sh

--- 00_scripts/13_blast_tomate.sh
+++ 00_scripts/13_blast_tomate.sh
@@ -8,3 +8,10 @@
 #SBATCH --error="/home/amartin3/CapoSagro_Alexa/00_scripts/13_blast_tomate.err"
 #SBATCH --output="/home/amartin3/CapoSagro_Alexa/00_scripts/13_blast_tomate.out"
 
+# Chargement de l'environnement
+module load conda/4.12.0
+source ~/.bashrc
+conda activate bioinformatic
+
+
+KRAKEN_FILE="home/amartin3/07_kraken2/clean_sed6_concat_dedup_merged.kraken"
