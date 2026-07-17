a2cdf10d8f18276394564f7ed68cc77a4e854e42
alexa923 <alexa.martin.thiers@gmail.com>
2026-07-17 10:18:26 +0200

    Update 13_blast_tomate.sh

--- 00_scripts/13_blast_tomate.sh
+++ 00_scripts/13_blast_tomate.sh
@@ -13,5 +13,26 @@ module load conda/4.12.0
 source ~/.bashrc
 conda activate bioinformatic
 
+#git clone https://github.com/jenniferlu717/KrakenTools.git
 
 KRAKEN_FILE="home/amartin3/07_kraken2/clean_sed6_concat_dedup_merged.kraken"
+FASTQ_FILE="home/amartin3/05_fastp/clean_sed6_concat_dedup_fastp_merged.fastq.gz"
+TAXID=4081
+
+
+echo "Début de l'extraction des reads (TaxID: ${TAXID}) - $(date)"
+
+# Extraction des reads avec KrakenTools
+extract_kraken_reads.py \
+    -k "$KRAKEN_FILE" \
+    -s "$FASTQ_FILE" \
+    -o "reads_tomate.fastq" \
+    -t "$TAXID" \
+    --include-children
+
+echo "Extraction terminée. Conversion au format FASTA - $(date)"
+
+# Conversion du FASTQ en FASTA pour le BLAST
+awk 'NR%4==1{sub(/^@/ ,">");print;getline;print}' reads_tomate.fastq > reads_tomate.fasta
+
+echo "Conversion terminée. Fin du job - $(date)"
