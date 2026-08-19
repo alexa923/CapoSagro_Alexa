501338f5255e9e7ad2ad89411e3f276e58990f26
alexa923 <alexa.martin.thiers@gmail.com>
2026-08-19 10:08:48 +0200

    Update 13_blast_Vitis_vinifera_sed6.sh

--- 00_scripts/13_blast_Vitis_vinifera_sed6.sh
+++ 00_scripts/13_blast_Vitis_vinifera_sed6.sh
@@ -35,7 +35,7 @@ cd /home/amartin3/07_kraken2
 #3 lancement du blast
 
 blastn -num_threads 64 -query "/home/amartin3/07_kraken2/sortie_sed6.fasta" \
-                -db /storage/biodatabanks/ncbi/NT/ncbi_blast_nt_2024-8-24/flat/nt -outfmt "6" \
+                -db /storage/biodatabanks/ncbi/NT/ncbi_blast_nt_2024-8-24/flat/nt -outfmt "5" \
                 -evalue 1e-3 \
                 -max_target_seqs 30 \
-                -out vigne_sed6.blastn
+                -out vigne_sed6_bis.blastn
